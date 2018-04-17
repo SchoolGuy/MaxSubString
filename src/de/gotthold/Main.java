58f7ec790a02efd97105957f2e6507aae949be6e
Enno Gotthold <matrixfueller@gmail.com>
2018-04-17 08:06:12 +0200

    Added project files

---
+++ src/de/gotthold/Main.java
@@ -0,0 +1,8 @@
+package de.gotthold;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+    }
+}
---
+++ src/de/gotthold/MaxSubString.java
@@ -0,0 +1,4 @@
+package de.gotthold;
+
+public class MaxSubString {
+}
